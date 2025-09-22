bcc66348b8a77d94b968e720fb26d31d28c7879c
RuinedVLR <rzhurakhov@gmail.com>
2025-09-22 14:59:07 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+}
