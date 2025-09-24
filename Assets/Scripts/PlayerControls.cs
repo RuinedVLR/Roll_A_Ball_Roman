0c6568068c1317ea5e5d49e8831096c45d17eb76
RuinedVLR <rzhurakhov@gmail.com>
2025-09-24 10:47:08 -0300

    Randomized Spawner

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -53,4 +53,14 @@ public class PlayerControls : MonoBehaviour
 
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
 }
