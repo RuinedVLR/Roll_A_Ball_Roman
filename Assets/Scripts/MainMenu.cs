ad2cba8504e86e5f915478a2b914fb01e1370491
RuinedVLR <rzhurakhov@gmail.com>
2025-11-03 19:47:57 -0400

    Upgrade save fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,10 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LVLupMenu.magnetLvl = 1;
+        LVLupMenu.speedLvl = 1;
+        LVLupMenu.fireRateLvl = 1;
+        LVLupMenu.firePowerLvl = 1;
     }
 
     public void QuitGame()
